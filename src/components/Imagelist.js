1af77922b1ed9ec1b7870f0ef3eae11a1ded2f6d
Pranav VK <pranavvk16@gmail.com>
2021-09-19 21:39:47 +0530

    key

--- src/components/Imagelist.js
+++ src/components/Imagelist.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 
 const Imagelist = (props) => {
-
+// map through all of the images object to display images seperatly
 console.log(props.images);
     const images = props.images.map((image) => {
 
-        return (<a href={image.webformatURL}><img key={image.id} style={{ width: '400px', height: '251px', margin: '20px' }} src={image.webformatURL} alt="img" /></a>)
+        return (<a className="imagelist" key={image.id} href={image.webformatURL}><img  style={{ width: '400px', height: '251px', margin: '20px' }} src={image.webformatURL} alt="img" /></a>)
     })
 
     return (
-        <center>{images}
+        <center className="imagelist-center">{images}
         </center>)
 }
 
